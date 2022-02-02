df7efbd6f090567ab22b53b61d2bfe9fd2a8cc26
Mahi2255 <87000485+Mahi2255@users.noreply.github.com>
2022-02-02 10:41:34 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -34,7 +34,7 @@ async def start(bot, update):
           disable_web_page_preview=True,
           reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='⭕ Cʜᴀɴɴᴇʟ ⭕', url=f'https://t.me/{Config.UPDATE_CHANNEL}'),
                                                  InlineKeyboardButton(text='⭕ Sᴜᴘᴘᴏʀᴛ ⭕', url=f'https://t.me/{Config.UPDATE_GROUP}') ],
-                                               [ InlineKeyboardButton(text='👨‍💻 Dᴇᴘʟᴏʏ Nᴏᴡ', url='https://t.me/Moviedroidnew'),                                                
+                                               [ InlineKeyboardButton(text='👨‍💻 JOIN US', url='https://t.me/Moviedroidnew'),                                                
                                                  InlineKeyboardButton(text='🔐 Cʟᴏꜱᴇ 🔐', callback_data='DM') ] ] ) )
 
 
